bc261569c889f752ca2f2384314d28960119695c
naroa <n.manterola@opendeusto.es>
2024-10-17 20:22:18 +0200

    VentanaProductoUsuario y clase Producto - Sin Terminar

--- src/domain/Producto.java
+++ src/domain/Producto.java
@@ -1,5 +1,61 @@
 package domain;
 
-public class Producto {
+import javax.swing.ImageIcon;
 
+public class Producto {
+	
+	protected String nombre;
+	//NO SE SI SE QUIERE INT O STRING, si eso cambiarlo
+	protected String id;
+	protected enum tipo{VerduraFruta, CarnePescado, ProductosSecos, Bebidas, Desayuno};
+	protected float precio;
+	protected String foto;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	
+	public Producto(String nombre, String id, float precio, String foto) {
+		super();
+		this.nombre = nombre;
+		this.id = id;
+		this.precio = precio;
+		this.foto = foto;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Producto [nombre=" + nombre + ", id=" + id + ", precio=" + precio+ "]";
+	}
+	
+	
+	
+	
 }
--- src/gui/VentanaProductoUsuario.java
+++ src/gui/VentanaProductoUsuario.java
@@ -1,5 +1,63 @@
 package gui;
 
-public class VentanaProductoUsuario {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
 
+import domain.Producto;
+import javax.swing.*;
+
+public class VentanaProductoUsuario extends JFrame{
+	
+	protected JLabel nombreProducto;
+	protected JLabel precioProducto;
+	protected JLabel cantidadProducto;
+	protected ImageIcon imagenProducto;
+	protected JLabel lblFoto;
+	protected JButton btnVolver;
+	protected JButton btnAnyadirWish;
+	protected JButton btnAnyadirCesta;
+	protected JPanel pnlIzq;
+	protected JPanel pnlDerecha;
+	protected JPanel pnlBotones;
+	protected JSpinner spinnerCantidad;
+	
+	public VentanaProductoUsuario(Producto p) {
+		inicializar(p);
+	}
+	
+	public void inicializar(Producto p) {
+		nombreProducto = new JLabel("Nombre: " + p.getNombre());
+		precioProducto = new JLabel("Precio: "+p.getPrecio());
+		cantidadProducto = new JLabel("Canridad: "+ new JSpinner());
+		imagenProducto = new ImageIcon(p.getFoto());
+		lblFoto = new JLabel(imagenProducto);
+		pnlIzq = new JPanel();
+		pnlDerecha = new JPanel();
+		pnlBotones = new JPanel();
+		btnAnyadirCesta = new JButton("AÑADIR A LA CESTA");
+		btnAnyadirWish = new JButton("AÑADIR A FAVORITOS");
+		btnVolver = new JButton("VOLVER");
+		
+		
+		pnlIzq.add(lblFoto);
+		pnlDerecha.setLayout(new GridLayout(0, 1));
+		pnlDerecha.add(nombreProducto);
+		pnlDerecha.add(precioProducto);
+		pnlDerecha.add(cantidadProducto);
+		pnlBotones.add(btnAnyadirCesta);
+		pnlBotones.add(btnAnyadirWish);
+		pnlBotones.add(btnVolver);
+		pnlDerecha.add(pnlBotones);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		//aqui habria que ponerle la ventana de que podructo se abre
+		//falta indicar el parametro de entrada
+		new VentanaProductoUsuario(new Producto("Producto1", "id1",9.99f, "/imagenes/IMG-20231017-WA0003.jpg"));
+		
+	}
 }
