306ecd053188f5907d7a387a200034e048e1a3b4
Jon <jon.r@opendeusto.es>
2024-10-18 22:58:50 +0200

    Tabla cesta del usuario hecha

---
+++ src/gui/ModeloCestaUsuario.java
@@ -0,0 +1,91 @@
+package gui;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
+
+import domain.Producto;
+
+public class ModeloCestaUsuario extends DefaultTableModel{
+
+	
+	private List<Producto> listaProductos;
+	private List<String> listaTitulos = Arrays.asList("ID", "Tipo", "Precio", "Tipo de IVA");
+	
+	
+	
+	public ModeloCestaUsuario(List<Producto> lp) {
+		listaProductos = lp; 		
+	}
+
+
+
+	@Override
+	public void addRow(Object[] rowData) {
+		// TODO Auto-generated method stub
+		super.addRow(rowData);
+	}
+
+
+
+	@Override
+	public void removeRow(int row) {
+		// TODO Auto-generated method stub
+		super.removeRow(row);
+	}
+
+
+
+	@Override
+	public int getRowCount() {
+		if(listaProductos == null) {
+			return 0;
+		}else {
+			return listaProductos.size();
+		}
+	}
+
+
+
+	@Override
+	public int getColumnCount() {
+		
+		return listaTitulos.size();
+	}
+
+
+
+	@Override
+	public String getColumnName(int column) {
+		// TODO Auto-generated method stub
+		return listaTitulos.get(column);
+	}
+
+
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		// TODO Auto-generated method stub
+		Producto p = listaProductos.get(row);
+		switch (column){
+			case 0: p.getId();
+			case 1: p.getNombre();
+			case 2: p.getPrecio();
+			case 3: p.getFoto();
+			default: return null;
+		}
+	}
+
+
+
+	@Override
+	public void setValueAt(Object aValue, int row, int column) {
+		// TODO Auto-generated method stub
+		super.setValueAt(aValue, row, column);
+	}
+	
+	
+	
+	
+}
--- src/gui/VentanaCestaUsuario.java
+++ src/gui/VentanaCestaUsuario.java
@@ -1,7 +1,58 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
+import domain.Producto;
 
 public class VentanaCestaUsuario extends JFrame{
+	private ModeloCestaUsuario modeloCestaUsuario;
+	private JTable tabla;
+	private JScrollPane scrolltabla;
+		
+	public VentanaCestaUsuario() {
+		super();
+		setBounds(300, 200, 600, 400);
+		
+		List<Producto> productos = new ArrayList<>();
+		modeloCestaUsuario = new ModeloCestaUsuario(productos);
+		tabla = new JTable(modeloCestaUsuario);
+		scrolltabla = new JScrollPane(tabla);
+		
+		getContentPane().add(scrolltabla, BorderLayout.CENTER);
+		
+		
+		
+		tabla.setDefaultRenderer(Object.class, new TableCellRenderer() {
+			
+			@Override
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+					int row, int column) {
+				
+				return null;
+			}
+		});
+		
+		
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		new VentanaCestaUsuario();
+		
 
+	
+	}
+	
 }
