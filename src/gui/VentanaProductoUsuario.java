e5c4c3918339ad63f241c4b59205734bc4a6c48c
naroa <n.manterola@opendeusto.es>
2024-10-23 15:39:09 +0200

    VentanaProductoUsuario

--- src/gui/VentanaProductoUsuario.java
+++ src/gui/VentanaProductoUsuario.java
@@ -23,12 +23,11 @@ public class VentanaProductoUsuario extends JFrame{
 	protected JPanel pnlBotones;
 	protected JSpinner spinnerCantidad;
 	
-	public VentanaProductoUsuario(Producto p) {
-		inicializar(p);
-	}
-	
-	public void inicializar(Producto p) {
-		nombreProducto = new JLabel("Nombre: " + p.getNombre());
+	public VentanaProductoUsuario(/**Producto p**/) {
+		
+		setBounds(200, 100, 500, 500);
+		
+		/**nombreProducto = new JLabel("Nombre: " + p.getNombre());
 		precioProducto = new JLabel("Precio: "+p.getPrecio());
 		cantidadProducto = new JLabel("Canridad: "+ new JSpinner());
 		imagenProducto = new ImageIcon(p.getFoto());
@@ -38,10 +37,22 @@ public class VentanaProductoUsuario extends JFrame{
 		pnlBotones = new JPanel();
 		btnAnyadirCesta = new JButton("AÑADIR A LA CESTA");
 		btnAnyadirWish = new JButton("AÑADIR A FAVORITOS");
+		btnVolver = new JButton("VOLVER");**/
+		
+		nombreProducto = new JLabel("Nombre: " );
+		precioProducto = new JLabel("Precio: ");
+		cantidadProducto = new JLabel("Cantidad: ");
+		//imagenProducto = new ImageIcon(p.getFoto());
+		//lblFoto = new JLabel(imagenProducto);
+		pnlIzq = new JPanel();
+		pnlDerecha = new JPanel();
+		pnlBotones = new JPanel();
+		btnAnyadirCesta = new JButton("AÑADIR A LA CESTA");
+		btnAnyadirWish = new JButton("AÑADIR A FAVORITOS");
 		btnVolver = new JButton("VOLVER");
 		
 		
-		pnlIzq.add(lblFoto);
+		//pnlIzq.add(lblFoto);
 		pnlDerecha.setLayout(new GridLayout(0, 1));
 		pnlDerecha.add(nombreProducto);
 		pnlDerecha.add(precioProducto);
@@ -50,14 +61,17 @@ public class VentanaProductoUsuario extends JFrame{
 		pnlBotones.add(btnAnyadirWish);
 		pnlBotones.add(btnVolver);
 		pnlDerecha.add(pnlBotones);
-		
+		getContentPane().add(pnlDerecha, BorderLayout.EAST);
+		getContentPane().add(pnlIzq, BorderLayout.WEST);
+		setVisible(true);
 	}
 	
 	
 	public static void main(String[] args) {
 		//aqui habria que ponerle la ventana de que podructo se abre
 		//falta indicar el parametro de entrada
-		new VentanaProductoUsuario(new Producto("Producto1", "id1",9.99f, "/imagenes/IMG-20231017-WA0003.jpg"));
+		/**new VentanaProductoUsuario(new Producto("Producto1", "id1",9.99f, "/imagenes/IMG-20231017-WA0003.jpg"));**/
+		new VentanaProductoUsuario();
 		
 	}
 }
