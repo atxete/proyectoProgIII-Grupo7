377e2bbb811b2804713a65635f3579ef91400c45
naroa <n.manterola@opendeusto.es>
2024-10-26 12:12:12 +0200

    VentanaPrincipalUsuario inicializado con un JLabel para ver si funciona listener de VentanProductoUsuario

--- src/gui/VentanaPrincipalUsuario.java
+++ src/gui/VentanaPrincipalUsuario.java
@@ -1,5 +1,24 @@
 package gui;
 
-public class VentanaPrincipalUsuario {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class VentanaPrincipalUsuario extends JFrame{
+	
+	/**ESTO LO HE ESCRITO PARA VER SI EL ACTION LISTENER DE 
+	 * MI VENTANA FUNCIONA Y ABRE ESTA VENTANA**/
+	
+	JLabel lblPrueba;
+	public VentanaPrincipalUsuario(){
+		setBounds(200, 50, 400, 400);
+		setTitle("Ventana Principal Usuario");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		lblPrueba = new JLabel("LABEL DE PRUEBA");
+		getContentPane().add(lblPrueba);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new VentanaPrincipalUsuario();
+	}
 }
