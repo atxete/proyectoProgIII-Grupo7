35214ef9062be8299624ff5167965f5a0dbbb12f
Etxekoa2 <Etxekoa2@10.164.53.10>
2024-10-29 11:14:52 +0100

    ventanProductoAdmin errores quitados + VentanaPrincipalAdmin

--- src/gui/VentanaPrincipalAdmin.java
+++ src/gui/VentanaPrincipalAdmin.java
@@ -1,26 +1,70 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
 
-public class VentanaPrincipalAdmin extends JFrame{
+import domain.Producto;
 
-	/**ESTO LO HE ESCRITO PARA VER SI EL ACTION LISTENER DE 
-	 * MI VENTANA FUNCIONA Y ABRE ESTA VENTANA**/
+public class VentanaPrincipalAdmin extends JFrame{
 	
-	JLabel lblPrueba;
+	protected JPanel pnlProductos;
+	protected JButton btnAnyadirProd;
+	protected JPanel pnlBotones;
 	
 	public VentanaPrincipalAdmin() {
 		
-		setBounds(200, 50, 400, 400);
-		setTitle("Ventana Principal Administrador");
+		setBounds(300, 100, 700, 500);
+		setTitle("Ventana Principal Admin");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		lblPrueba = new JLabel("LABEL DE PRUEBA");
-		getContentPane().add(lblPrueba);
+		setLayout(new BorderLayout());
+		
+		
+		//cargamos los productos (funcion que generara una lista de productos)
+		/* 
+		 * productos = supermercado.crearProductos()
+		 */
+		List<Producto> productos = new ArrayList();
+		productos.add(new Producto("P1", "id1", 1, "foto1"));
+		
+		pnlProductos = new JPanel(new GridLayout(2, productos.size()));
+		
+		for(Producto p: productos) {
+			JLabel nombreProducto = new JLabel("Nombre: " + p.getNombre());
+			JLabel precioProducto = new JLabel("Precio: "+p.getPrecio());
+			
+			//ImageIcon imagenProducto = new ImageIcon(p.getFoto());
+			//JLabel lblFoto = new JLabel(imagenProducto);
+			
+			JPanel pnlPrincipal = new JPanel();
+			JPanel pnlCentral = new JPanel();
+	        
+	        pnlCentral.add(nombreProducto);
+	        pnlCentral.add(precioProducto);
+	        
+	        //pnlPrincipal.add(lblFoto, BorderLayout.NORTH);
+	        pnlPrincipal.add(pnlCentral, BorderLayout.CENTER);
+	        
+	        pnlProductos.add(pnlPrincipal);
+		}
+		
+		btnAnyadirProd = new JButton("Añadir Producto");
+		
+		pnlBotones = new JPanel();
+		
+		pnlBotones.add(btnAnyadirProd);
+		this.add(pnlProductos, BorderLayout.CENTER);
+		this.add(pnlBotones, BorderLayout.SOUTH);
+		
 		
 		setVisible(true);
 	}
 	public static void main(String[] args) {
-		new VentanaPrincipalUsuario();
+		new VentanaPrincipalAdmin();
+		
 	}
 }
