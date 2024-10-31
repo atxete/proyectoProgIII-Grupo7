ca94ad36682be08d91a02414ebaabb30adc94583
naroa <n.manterola@opendeusto.es>
2024-10-31 15:35:11 +0100

    w

--- src/domain/Administrador.java
+++ src/domain/Administrador.java
@@ -4,5 +4,6 @@ public class Administrador extends Usuario{
 
 	public Administrador(String nombre, String apellidos, String usuario, String contrasenya, String email) {
 		super(nombre, apellidos, usuario, contrasenya, email, 1);
+		
 	}
 }
