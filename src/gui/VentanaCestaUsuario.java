00aec54ea2a66d1f769bcd099c27c45578d59e6c
Jon <jon.r@opendeusto.es>
2024-11-11 08:33:21 +0100

    cambio

--- src/gui/VentanaCestaUsuario.java
+++ src/gui/VentanaCestaUsuario.java
@@ -2,8 +2,6 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
