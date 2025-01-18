38da88ee0f8d5233537d64c6d39f82f1499ec19d
naroa <n.manterola@opendeusto.es>
2025-01-18 12:26:37 +0100

    cambios

--- src/domain/Producto.java
+++ src/domain/Producto.java
@@ -87,7 +87,13 @@ public class Producto {
 	}
 
 	
-	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		if(obj == null || getClass() != obj.getClass()) return false;
+		Producto producto = (Producto) obj;
+		return nombre.equals(producto.nombre); //si tienen el mismo nombre son el mismo producto
+	}
 	
 	
 	
